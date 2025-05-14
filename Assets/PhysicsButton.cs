09709798593b7780e81eb1ded7a80ce994144a17
GOL64 <gardolavlangs@gmail.com>
2025-05-14 13:39:20 +0200

    Followed button tutorial

--- Assets/PhysicsButton.cs
+++ Assets/PhysicsButton.cs
@@ -1,16 +1,58 @@
 using UnityEngine;
+using UnityEngine.Events;
 
 public class PhysicsButton : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public float threshold = 0.1f;
+    public float deadZone = 0.025f;
+
+    private bool isPressed;
+    private Vector3 startPos;
+    private ConfigurableJoint joint;
+    public UnityEvent onPressed, onReleased;
+    
     void Start()
     {
-        
+        startPos =  transform.localPosition;
+        joint = GetComponent<ConfigurableJoint>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (!isPressed && GetValue() + threshold>= 1)
+        { 
+            Pressed();
+        }
+        if (!isPressed && GetValue() - threshold <= 0)
+        {
+            Released();
+        }
+    }
+
+    private float GetValue()
+    {
+        var value = Vector3.Distance(startPos, transform.localPosition) / joint.linearLimit.limit;
+
+        if(Math.Abs(value) < deadZone)
+        {
+            value = 0;
+        }
+
+        return Mathf.Clamp(value, -1f, 1f);
+    }
+
+     private void Pressed()
+    {
+        isPressed = true;
+        onPressed.Invoke();
+        Debug.Log("Pressed");
+    }
+
+    private void Released()
+    {
+        isPressed = false;
+        onReleased.Invoke();
+        Debug.Log("rELEASED ");
     }
 }
